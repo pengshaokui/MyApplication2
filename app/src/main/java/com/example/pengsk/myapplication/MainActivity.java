d18abe02757a8f8d0641393b3d9a7d13862b681e
shaokui <524490971@qq.com>
2018-03-15 10:58:07 +0800

    aa

--- app/src/main/java/com/example/pengsk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pengsk/myapplication/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         final TextView text = findViewById(R.id.text);
         ShareSDK.initSDK(this);
