508875481065a218b04ac9d103824a0275c96d46
shaokui <524490971@qq.com>
2018-03-15 11:42:26 +0800

    aa

--- app/src/main/java/com/example/pengsk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pengsk/myapplication/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 //            @Override
 //            public void onClick(View view) {
 //                int  d = new Random().nextInt(3);
+
 //                text.setText(d + "   " + (int) d);
 //            }
 //        });
